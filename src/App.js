016054a6494f003f1c3b8a598bcadce9ff6804f6
Ninad <ninadkasture98@gmail.com>
2025-03-12 10:25:17 +0530

    just initial learnings

---
+++ src/App.js
@@ -0,0 +1,8 @@
+ import react from 'react';
+ import reactDom from 'react-dom';
+
+ function myButton(){
+  const link="www.google.com"
+  return <a href={link}>click me</a>;
+ }
+ export default myButton;
